52efc6d64098a82e876341afb3943df122d4dbac
Byungchan Lim <bl458@cornell.edu>
2020-09-07 21:46:49 +0900

    feat: Implement auth service, module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { randomBytes } from 'crypto';
+import { hash, compare } from 'bcrypt';
+
+const TOKEN_BYTES = 256;
+const SALT_ROUNDS = 12;
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async generateToken(): Promise<string> {
+    return new Promise((resolve, reject) =>
+      randomBytes(TOKEN_BYTES, (err, buff) => {
+        err ? reject(err) : resolve(buff.toString('hex'));
+      }),
+    );
+  }
+
+  async hashPw(pw: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      hash(pw, SALT_ROUNDS, (err, encrypted) => {
+        err ? reject(err) : resolve(encrypted);
+      });
+    });
+  }
+
+  async comparePw(pw: string, hashedPw: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      compare(pw, hashedPw, (err, bool) => {
+        err ? reject(err) : resolve(bool);
+      });
+    });
+  }
+}
