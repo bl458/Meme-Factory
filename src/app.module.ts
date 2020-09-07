7af522220107e118ab480315f6e79f5e6c2d7fb9
Byungchan Lim <bl458@cornell.edu>
2020-09-07 21:18:03 +0900

    feat: Create placeholders

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from './auth/auth.module';
+import { ImageModule } from './image/image.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ImageModule, UserModule],
 })
 export class AppModule {}
