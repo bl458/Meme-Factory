9e6211bc6d54ae38a69645caa2077b471ca87200
Byungchan Lim <bl458@cornell.edu>
2020-09-09 19:31:17 +0900

    feat: Implement email pipe with validator library

--- src/helper/pipes/EmailPipe.ts
+++ src/helper/pipes/EmailPipe.ts
@@ -1,32 +1,28 @@
 import {
   PipeTransform,
-  Injectable,
   ArgumentMetadata,
   BadRequestException,
 } from '@nestjs/common';
 
 import { isString } from 'util';
+
 import validator from 'validator';
 
-@Injectable()
-export class TokenPipe implements PipeTransform {
+export class EmailPipe implements PipeTransform {
   transform(value: any, { data, metatype }: ArgumentMetadata): any {
     if (value === undefined)
       throw new BadRequestException(`${data} is required.`);
 
     if (metatype !== String)
-      throw Error(
-        `${data} has wrong type; ${String} expected, got ${metatype}`,
+      throw new Error(
+        `${data} has wrong type. Expected ${String} but got ${metatype}.`,
       );
 
     if (!isString(value))
       throw new BadRequestException(`${data} must be a string.`);
 
-    if (!validator.isAlphanumeric(value))
-      throw new BadRequestException(`${data} must be alphanumeric.`);
-
-    if (value.length !== 256)
-      throw new BadRequestException(`${data} must be a valid token.`);
+    if (!validator.isEmail(value))
+      throw new BadRequestException(`${data} must be an email.`);
 
     return value;
   }
