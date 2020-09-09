64d4ae338d3de9fb675568f2182a49b554c2700f
Byungchan Lim <bl458@cornell.edu>
2020-09-09 21:00:24 +0900

    fix: Fix user module imports

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -6,7 +6,11 @@ import { UserSessionController } from './user.session.controller';
 import { UserService } from './user.service';
 import { UserSessionService } from './user.session.service';
 
+import { AuthModule } from 'src/auth/auth.module';
+import { DBModule } from 'src/db/db.module';
+
 @Module({
+  imports: [AuthModule, DBModule],
   controllers: [UserController, UserSessionController],
   providers: [UserService, UserSessionService],
 })
