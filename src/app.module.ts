5b088234f95505178dc46e214f0946238ab2d35c
Byungchan Lim <bl458@cornell.edu>
2020-09-09 20:59:08 +0900

    fix: Fix user, app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ImageModule } from './image/image.module';
 import { UserModule } from './user/user.module';
+import { DBModule } from './db/db.module';
 
 @Module({
-  imports: [AuthModule, ImageModule, UserModule],
+  imports: [AuthModule, ImageModule, UserModule, DBModule],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
 import { UserController } from './user.controller';
+import { UserSessionController } from './user.session.controller';
 
 import { UserService } from './user.service';
+import { UserSessionService } from './user.session.service';
 
 @Module({
-  controllers: [UserController],
-  providers: [UserService],
+  controllers: [UserController, UserSessionController],
+  providers: [UserService, UserSessionService],
 })
 export class UserModule {}
