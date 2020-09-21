c7d651350cf560a06c158b60f45e4d59d201bf88
Byungchan Lim <bl458@cornell.edu>
2020-09-21 17:08:41 +0900

    fix: Fix admin image controller to not stop uploading on one failed

--- src/admin/admin.image.controller.ts
+++ src/admin/admin.image.controller.ts
@@ -29,17 +29,25 @@ export class AdminImageController {
     if (files.length === 0) throw new BadRequestException('no files');
 
     let result = [];
+    let failed = [];
 
     for (let i = 0; i < files.length; i++) {
-      console.log(`\nUploading ${i}th file ${files[i].originalname}\n`);
-
       if (!files[0].mimetype.includes('image/'))
         throw new BadRequestException(`${i}th file is not an image`);
 
-      const newImage = await this.iuService.uploadNewImage(session, files[i]);
-
-      result = [...result, newImage];
+      try {
+        const newImage = await this.iuService.uploadNewImage(session, files[i]);
+        result = [...result, newImage];
+        console.log(
+          `\nUploaded file[${i}] ${files[i].originalname}\n${newImage.url}\n`,
+        );
+      } catch (err) {
+        console.log(`\nFailed to upload file[${i}] ${files[i].originalname}\n`);
+        failed = [...failed, `${i}th file ${files[i].originalname}: ${err}`];
+      }
     }
+
+    if (failed.length > 1) console.log(failed);
 
     return result;
   }
