f069fa4badd63f5fca7de2df872eead42699a507
Byungchan Lim <bl458@cornell.edu>
2020-09-30 15:56:07 +0900

    feat: Add cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
 
   if (process.env.LOAD_ENV === 'YES')
     dotenv.config({ path: __dirname + '/.env' });
 
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
